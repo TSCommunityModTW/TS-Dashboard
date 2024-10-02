ffa0a1ac406971b6133fc3ac5fc17e3336a6f4f2
Yu <quasipersonal58@gmail.com>
2024-10-02 21:17:38 +0800

    Flx: Get data error

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,5 +1,5 @@
 export const configProd = {
-    API_LOCATION: "http://localhost:8030",
+    API_LOCATION: "http://api.tshosts.com:9042",
     S3_TS_LAUNCHER_METSDATA: "http://s3api.tshosts.com:9800/ts-launcher-metadata",
-    SOCKET_LOCATION: "ws://localhost:8030"
+    SOCKET_LOCATION: "ws://api.tshosts.com:9042"
 }
